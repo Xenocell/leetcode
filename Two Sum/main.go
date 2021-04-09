16049f57e690d27131972322b839f11c398c494f
Xenocell <rassa.ork@mail.ru>
2021-04-09 21:29:03 +0300

    Create main.go

---
+++ Two Sum/main.go
@@ -0,0 +1,12 @@
+func twoSum(nums []int, target int) []int {
+	for i1, v1 := range nums {
+		for i2, v2 := range nums {
+			if i1 == i2 {
+				continue
+			} else if (v1 + v2) == target {
+				return []int{i1, i2}
+			}
+		}
+	}
+	return []int{}
+}
